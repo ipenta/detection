3bdbcee99aab0a321dda1131f122b5eb8468fc93
yiyern <yiyern@yahoo.com>
2018-07-29 17:44:35 +0800

    account

--- src/store/modules/account.js
+++ src/store/modules/account.js
@@ -1,15 +1,23 @@
 import * as userService from '@/service/sys/user'
+import AccountVO from '@/service/model/sys/AccountVO'
+import * as types from '@/store/mutation-type'
 
-const state = {}
+let userId = window.sessionStorage.userId || ''
+
+const state = new AccountVO(userId)
 const getters = {}
 const actions = {
   logon({ commit, state }, payload) {
     return userService.findOne(payload).then(account => {
-      console.log(account)
+      commit(types.SET_ACCOUNT, account)
     })
   }
 }
-const mutations = {}
+const mutations = {
+  [types.SET_ACCOUNT] (state, data) {
+    Object.assign(state, data)
+  }
+}
 
 export default {
   state,
